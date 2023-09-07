60bdb17249b385bbcb10289ffe189792a87a5a1f
James Underwood <jimnastic316@gmail.com>
2023-09-07 15:16:22 -0400

    Sect198 Loops
    Adding Loops and stuff

---
+++ Sect19RepeatingStuffWithLoops/197LoopingOverArrays/app.js
@@ -0,0 +1,11 @@
+const animals = ['lions', 'tigers', 'bears'];
+
+for (let i = 0; i < animals.length; i++){
+    console.log(i, animals[i]);
+}
+
+//reverse 
+for (let i = animals.length - 1 ; i >= 0; i--){
+    console.log(i, animals[i]);
+}
+
