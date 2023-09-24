077c45e25a6033c4b9948d99d707491ca539b158
James Underwood <jimnastic316@gmail.com>
2023-09-24 14:54:51 -0400

    Sect 221 This
    started

--- Sect20NewIntroducingFunctions/Practice/app.js
+++ Sect20NewIntroducingFunctions/Practice/app.js
@@ -30,17 +30,17 @@ console.log(lastElement(emptArr));
 
 let word = 'abcdefghij';
 
-function capitalize (word) {
-//    console.log(word.toUpperCase());
+function capitalize(word) {
+    //    console.log(word.toUpperCase());
     let firstLetter = word[0].toUpperCase();
     let tail = (word.length);
-//    console.log(tail);
+    //    console.log(tail);
     let end = word.slice(1, tail);
-//    console.log(end);
+    //    console.log(end);
     let firstIsCapital = (firstLetter + end);
-//    console.log(firstLetter + end);
+    //    console.log(firstLetter + end);
     return (firstIsCapital);
-    
+
 }
 
 capitalize(word);
@@ -56,11 +56,11 @@ console.log(capitalize(word));
 //     sumArray([2,2,2,2]) // 8
 //     sumArray([50,50,1]) // 101
 
-const  arrayOfInts = [10, 10, 10, 10, 10, 10];
+const arrayOfInts = [10, 10, 10, 10, 10, 10];
 
 let sum = 0;
 
-function sumArray (arrayOfInts) {
+function sumArray(arrayOfInts) {
 
     // for (let i = 0; i < arrayOfInts.length; i++){
     //     let addTo = (arrayOfInts[i]);
@@ -70,18 +70,89 @@ function sumArray (arrayOfInts) {
 
     let total = 0;
     for (let sub of arrayOfInts) {
-        
+
         total = total + sub;
         console.log(total);
-        
+
     }
 
-    
+
     // return sum;
     return total;
 }
 
 sumArray(arrayOfInts);
+
+// ************************************
+// Days Of The Week Exercise
+
+// Write a function called returnDay. this function takes in one parameter (a number from 1-7) 
+// and returns the day of the week (1 is Monday, 2 is Tuesday, etc.)  If the number is less than 
+// 1 or greater than 7, the function should return null. In some countries Sunday is treated as the 
+// first day of the week, but for this exercise we will use Monday as the first day.
+
+day = null;
+function returnDay(num) {
+
+    // If Monday
+    if (num == 1) {
+        console.log('Day is 1');
+        day = 'Monday';
+    } else if (num == 2) {
+        console.log('Day is 2');
+        day = 'Tuesday';
+    } else if (num == 3) {
+        console.log('Day is 3');
+        day = 'Wednesday';
+    } else if (num == 4) {
+        console.log('Day is 4');
+        day = 'Thursday';
+    } else if (num == 5) {
+        console.log('Day is 5');
+        day = 'Friday';
+    } else if (num == 6) {
+        console.log('Day is 6');
+        day = 'Saturday';
+    } else if (num == 7) {
+        console.log('Day is 7');
+        day = 'Sunday';
+    } else if (num < 1 || num > 7) {
+        console.log('Day is null');
+        day = null;
+    }
+    return (day);
+}
+
+let answer = (returnDay(1));
+console.log(answer);
+
+answer = (returnDay(2));
+console.log(answer);
+
+answer = (returnDay(3));
+console.log(answer);
+
+answer = (returnDay(4));
+console.log(answer);
+
+answer = (returnDay(5));
+console.log(answer);
+
+answer = (returnDay(6));
+console.log(answer);
+
+answer = (returnDay(7));
+console.log(answer);
+
+answer = (returnDay(0));
+console.log(answer);
+
+answer = (returnDay(8));
+console.log(answer);
+
+
+
+  
 
 
 // const animals = ['lions', 'tigers', 'bears'];
---
+++ Sect21_LevelingUpOurFunctions/CodingExercise44FunctionExpressionExerciseise/app.js
@@ -0,0 +1,16 @@
+// Function Expression Exercise
+
+// Define a function that returns the square of a number. 
+// Define the function as a function expression, stored in a variable 
+// called square.  NOTE: Udemy's coding exercise platform does NOT support 
+// the ** operator. 
+
+//     square(4) //16
+//     square(3) //9
+
+const square = function (num) {
+    return (num * num);
+}
+
+console.log(square(4))
+console.log(square(3))
