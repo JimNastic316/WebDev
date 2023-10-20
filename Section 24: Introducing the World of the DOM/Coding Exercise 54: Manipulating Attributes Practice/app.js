aa25062d5470c328d826904b9eaabaf666eadec1
James Underwood <jimnastic316@gmail.com>
2023-10-20 15:29:37 -0400

    attribute and styles
    exercises in setting w/ js

---
+++ Section 24: Introducing the World of the DOM/Coding Exercise 54: Manipulating Attributes Practice/app.js
@@ -0,0 +1,11 @@
+// Let's get some practice working with DOM element attributes. 
+// I've provided you with some very basic markup.  
+// Please select the image element and:
+
+//     change its source to this url: https://devsprouthosting.com/images/chicken.jpg
+
+//     change its alt text to be "chicken"
+
+const imageElement = document.querySelector('img');
+imageElement.src = 'https://devsprouthosting.com/images/chicken.jpg';
+imageElement.alt = 'chicken';
